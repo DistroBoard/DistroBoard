61503866b2e4e3f675ca81583cfa2b6d0aaea744
Biswas Jishnu <biswasjishnu24@gmail.com>
2021-05-20 10:58:06 +0530

    setup basic app with frontend and backend.

---
+++ DistroBoard/client/src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div><h1>DistroBoard</h1></div>
+  );
+}
+
+export default App;
---
+++ DistroBoard/server/index.js
@@ -0,0 +1,9 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+const app = express();
+app.use(bodyParser.json({limit: '30mb', extended: true}));
+app.use(bodyParser.urlencoded({limit: '30mb', extended: true}));
+app.use(cors());
