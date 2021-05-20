2cbacd03a1a46b08017c0273aedde9cf5e2f25d7
Biswas Jishnu <biswasjishnu24@gmail.com>
2021-05-20 18:06:21 +0530

    imported connectDB module to connect to database.

--- DistroBoard/server/index.js
+++ DistroBoard/server/index.js
@@ -1,20 +1,26 @@
+//Import all dependencies
 import express from 'express';
 import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
 import cors from 'cors';
 
+//Create the app
 const app = express();
 
+//Allow our app to use the dependencies
 app.use(bodyParser.json({limit: '30mb', extended: true}))
 app.use(bodyParser.urlencoded({limit: '30mb', extended: true}))
 app.use(cors());
 
-
-const MONGO_CONNECTION_URL = 'mongodb+srv://distroboard:HULSAXa6277arc47@distroboard.j3j2x.mongodb.net/distroboard?retryWrites=true&w=majority";"mongodb+srv://distroboard:HULSAXa6277arc47@distroboard.j3j2x.mongodb.net/distroboard?retryWrites=true&w=majority';
+//Setup our port
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(MONGO_CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
-  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}\nDatabase Connection Succesful!`)))
-  .catch((error) => console.log(`${error}\nDatabase Connection Failed`));
+//Connect to our database
+import connectDB from './config/db.js';
+connectDB();
 
-mongoose.set('useFindAndModify', false);
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}.\nVisit http://localhost:${PORT} to view.`);
+});
+
+
