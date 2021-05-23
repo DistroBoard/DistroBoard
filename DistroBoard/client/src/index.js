1964f110e2b2ea91d5b1f4b92e0ded6fcd8b0a4a
Biswas Jishnu <biswasjishnu24@gmail.com>
2021-05-23 07:59:40 +0530

    connected application with redux

--- DistroBoard/client/src/index.js
+++ DistroBoard/client/src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware, compose} from 'redux';
+import thunk from 'redux-thunk';
 
+import reducers from './reducers';
 import App from './App';
 
+
+const store = createStore(reducers, compose(applyMiddleware(thunk)));
+
 ReactDOM.render(
-  <App />, document.getElementById('root'),
+  <Provider store={store}> <App /></Provider>,
+  document.getElementById('root')
 );
---
+++ DistroBoard/client/src/reducers/index.js
@@ -0,0 +1,4 @@
+import {combineReducers} from 'redux';
+import distros from './distros';
+
+export default combineReducers({distros});
