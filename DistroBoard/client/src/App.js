5fd7fe72b1e9a7ca193362d2dee2419c9d112071
Biswas Jishnu <biswasjishnu24@gmail.com>
2021-05-23 08:38:26 +0530

    fetch data from backend

--- DistroBoard/client/src/App.js
+++ DistroBoard/client/src/App.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Grow, Grid, Container} from '@material-ui/core';
+
+import {useDispatch} from 'react-redux';
+import {getDistros} from './actions/distros';
 
 import Header from './components/Header/Header';
 import Distros from './components/Distros/Distros';
 import Form from './components/Form/Form';
 
 const App = () => {
+
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getDistros());
+  }, [dispatch]);
+
   return (
     <>
       <Header />
--- DistroBoard/client/src/components/Distros/Distros.js
+++ DistroBoard/client/src/components/Distros/Distros.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import {useSelector} from 'react-redux';
+
 import Distro from './Distro/Distro';
 import useStyles from './styles';
 
 const Distros = () => {
+  const distros = useSelector((state) => state.distros);
   const classes = useStyles();
+  console.log(distros);
   return (
     <>
       <Distro />
