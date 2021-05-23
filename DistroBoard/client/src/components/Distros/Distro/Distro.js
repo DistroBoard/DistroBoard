85bd0b87bc1dbe1953dcc54eca21ec5cff73d87c
Biswas Jishnu <biswasjishnu24@gmail.com>
2021-05-23 13:45:02 +0530

    boiler card template

--- DistroBoard/client/src/components/Distros/Distro/Distro.js
+++ DistroBoard/client/src/components/Distros/Distro/Distro.js
@@ -1,10 +1,40 @@
 import React from 'react';
 import useStyles from './styles';
 
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+
 const Distro = () => {
   const classes = useStyles();
   return (
-    <h1>Distro Data</h1>
+    <>
+      <Card >
+        <CardContent>
+          <Typography color="textSecondary" gutterBottom>
+            Distro of the Day
+        </Typography>
+          <Typography variant="h5" component="h2">
+            Arch Linux
+        </Typography>
+          <Typography  >
+            The best distro in the world
+          </Typography>
+          <Typography variant="body2" component="p">
+            Use Arch
+          <br />
+            {'"Cuz WIndows suX"'}
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small">Learn More</Button>
+        </CardActions>
+      </Card>
+
+    </>
   );
 }
 
