51b1ad928b232cc581ad4fb765314d53bb1a2c4e
Biswas Jishnu <biswasjishnu24@gmail.com>
2021-05-23 13:38:16 +0530

    implemented custom color scheme and removed lightmode toggle

--- DistroBoard/client/src/index.js
+++ DistroBoard/client/src/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import {ThemeProvider, createMuiTheme} from '@material-ui/core/styles';
+import {CssBaseline} from '@material-ui/core';
+
 import {Provider} from 'react-redux';
 import {createStore, applyMiddleware, compose} from 'redux';
 import thunk from 'redux-thunk';
@@ -10,7 +14,27 @@ import App from './App';
 
 const store = createStore(reducers, compose(applyMiddleware(thunk)));
 
+const theme = createMuiTheme({
+  palette: {
+    type: "dark",
+    primary: {
+      main: '#90caf9',
+      //contrastText: '#fff',
+    },
+    secondary: {
+      main: '#f48fb1',
+    },
+  }
+});
+
 ReactDOM.render(
-  <Provider store={store}> <App /></Provider>,
+  <Provider store={store}>
+    <ThemeProvider theme={theme}>
+
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
+
+  </Provider>,
   document.getElementById('root')
 );
