d79fbae4e59f6cd8af3fb0993dd63043c7829882
Biswas Jishnu <biswasjishnu24@gmail.com>
2021-05-26 22:12:54 +0530

    modified header ui

--- DistroBoard/client/src/components/Header/Header.js
+++ DistroBoard/client/src/components/Header/Header.js
@@ -1,19 +1,22 @@
 import React from 'react';
-import {AppBar, Toolbar, Typography, IconButton, Tooltip} from '@material-ui/core';
+import {AppBar, Toolbar, Typography, IconButton, Tooltip, Avatar} from '@material-ui/core';
+
+import logo from "../../assets/images/logo.svg";
+import useStyles from './styles';
 
 import GitHubIcon from '@material-ui/icons/GitHub';
-import InfoIcon from '@material-ui/icons/Info';
-import MenuIcon from '@material-ui/icons/Menu';
-
+import CasinoIcon from '@material-ui/icons/Casino';
+import FavoriteIcon from '@material-ui/icons/Favorite';
 const Header = () => {
+  const classes = useStyles();
   return (
     <AppBar style={{marginBottom: 30}} color="default" position="static" >
       <Toolbar>
-
-        <Tooltip title="Menu" aria-label="menu"><IconButton color="inherit"><MenuIcon /></IconButton></Tooltip>
+        <Avatar alt="Logo" className={classes.logo} variant="square" src={logo} />
         <Typography variant="h6" style={{flexGrow: 1}}>DistroBoard</Typography>
 
-        <Tooltip title="About" aria-label="about"><IconButton color="inherit"><InfoIcon /></IconButton></Tooltip>
+        <Tooltip title="Distro Picker" aria-label="distro-picker"><IconButton color="inherit"><CasinoIcon /></IconButton></Tooltip>
+        <Tooltip title="Donate" aria-label="donate"><IconButton color="inherit"><FavoriteIcon /></IconButton></Tooltip>
         <Tooltip title="View Source" aria-label="source"><IconButton color="inherit" href="https://github.com/DistroBoard/DistroBoard"><GitHubIcon /></IconButton></Tooltip>
 
       </Toolbar >
