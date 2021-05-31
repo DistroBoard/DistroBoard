d61a3b8edd6c15918005c70dcc842b9228cf84d7
Jishnu Biswas <65299153+BiswasJishnu@users.noreply.github.com>
2021-05-31 13:45:36 +0530

    Fixed connection URL

--- DistroBoard/server/config/db.js
+++ DistroBoard/server/config/db.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-const MONGO_CONNECTION_URL = 'mongodb+srv://distroboard:HULSAXa6277arc47@distroboard.j3j2x.mongodb.net/distroboard?retryWrites=true&w=majority";"mongodb+srv://distroboard:HULSAXa6277arc47@distroboard.j3j2x.mongodb.net/distroboard?retryWrites=true&w=majority';
+const MONGO_CONNECTION_URL = "mongodb+srv://distroboard:HULSAXa6277arc47@distroboard.j3j2x.mongodb.net/distroboard?retryWrites=true&w=majority";
 
 export default function connectDB() {
   mongoose.connect(MONGO_CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false})
