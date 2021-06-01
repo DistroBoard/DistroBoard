2dd3c98e4c7c379415e45e09e36c5e60a3bc118c
Biswas Jishnu <biswasjishnu24@gmail.com>
2021-06-01 09:52:55 +0530

    fetch display info from db and display in cards

--- DistroBoard/client/src/components/Distros/Distros.js
+++ DistroBoard/client/src/components/Distros/Distros.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import {useSelector} from 'react-redux';
-
+import {Typography, Grid} from '@material-ui/core';
 import Distro from './Distro/Distro';
 import useStyles from './styles';
 
 const Distros = () => {
   const distros = useSelector((state) => state.distros);
   const classes = useStyles();
-  console.log(distros);
+
   return (
-    <>
-      <Distro />
-    </>
+    !distros.length ? <Typography variant="h6" style={{flexGrow: 1}}>Sorry we could not fetch anything. <br /> Please try again later.</Typography> : (
+      <Grid className={classes.mainContainer} container alignItems="stretch" spacing={3}>
+        {distros.map((distro) => (
+          <Grid key={distro._id} item xs={12} sm={6} md={6}>
+            <Distro distro={distro} />
+          </Grid>
+        ))}
+      </Grid>
+    )
   );
-}
+};
 
 export default Distros;
