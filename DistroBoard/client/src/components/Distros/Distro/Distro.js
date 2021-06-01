28285160e72529e59167f0a005be848d7f9dc82f
Biswas Jishnu <biswasjishnu24@gmail.com>
2021-06-01 09:52:25 +0530

    fetch display info from db and display in cards

--- DistroBoard/client/src/components/Distros/Distro/Distro.js
+++ DistroBoard/client/src/components/Distros/Distro/Distro.js
@@ -1,40 +1,47 @@
 import React from 'react';
 import useStyles from './styles';
 
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
 
+import {Card, CardActions, CardContent, CardMedia, IconButton, Typography, Avatar, Tooltip} from '@material-ui/core/';
 
-const Distro = () => {
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import DeleteIcon from '@material-ui/icons/Delete';
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
+
+const Distro = ({distro}) => {
   const classes = useStyles();
   return (
     <>
-      <Card >
+      <Card className={classes.card}>
+
+        <CardMedia className={classes.media} image={distro.distroScreenshot} title={distro.distroName} />
+
+        <div className={classes.overlay}>
+          <Typography variant="body2">{distro.distroName}</Typography>
+        </div>
+
+        <div className={classes.overlay2}>
+          <Tooltip title="Edit info" aria-label="edit"><IconButton size="small"><MoreHorizIcon fontSize="default" /></IconButton></Tooltip>
+        </div>
+
+        <div className={classes.details}>
+          <Avatar aria-label="logo" variant="square" className={classes.logo} src={distro.distroLogo} />
+        </div>
+
         <CardContent>
-          <Typography color="textSecondary" gutterBottom>
-            Distro of the Day
-        </Typography>
-          <Typography variant="h5" component="h2">
-            Arch Linux
-        </Typography>
-          <Typography  >
-            The best distro in the world
-          </Typography>
-          <Typography variant="body2" component="p">
-            Use Arch
-          <br />
-            {'"Cuz WIndows suX"'}
-          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">{distro.distroDescription}</Typography>
         </CardContent>
-        <CardActions>
-          <Button size="small">Learn More</Button>
+
+        <div className={classes.details}>
+          <Typography variant="body2" color="textSecondary" component="h2">{distro.tags.map((tag) => `#${tag} `)}</Typography>
+        </div>
+
+        <CardActions className={classes.cardActions}>
+          <Tooltip title="Like this distro" aria-label="like"><IconButton size="small" color="secondary"> <FavoriteIcon fontSize="small" /> </IconButton></Tooltip>
+          <Tooltip title="Delete this distro" aria-label="delete"><IconButton size="small" color="secondary"><DeleteIcon fontSize="small" /> </IconButton></Tooltip>
         </CardActions>
+
       </Card>
-
-
     </>
   );
 }
