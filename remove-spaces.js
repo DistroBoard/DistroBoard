2b1eef86b666bf013390e43654e0e85c9cbb9cc4
Shaurya <github@sandy007.anonaddy.com>
2021-06-02 16:35:49 +0530

    JS to remove spaces and make it lower case

---
+++ remove-spaces.js
@@ -0,0 +1,5 @@
+function myFunction() {
+  var distro = "A   R    C     H       L   I   N   U   X";
+  var res = str.toLowerCase().replace(/\s/g, '');
+  document.getElementById("demo").innerHTML = res;
+}
