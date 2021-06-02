15e95d67b5939cd11aaa19180fa9b70a9cf23465
Shaurya <github@sandy007.anonaddy.com>
2021-06-02 16:38:55 +0530

    Update remove-spaces.js

--- remove-spaces.js
+++ remove-spaces.js
@@ -1,5 +1,5 @@
-function myFunction() {
-  var distro = "A   R    C     H       L   I   N   U   X";
-  var res = str.toLowerCase().replace(/\s/g, '');
-  document.getElementById("demo").innerHTML = res;
+function removespaces() {
+  var distro = "A   R    C     H    L   I   N   U   X";
+  var res = str.toLowerCase().replace(/\s/g, '');         //result string to lower case and remove spaces
+  document.getElementById("demo").innerHTML = res;        //result will be archlinux
 }
