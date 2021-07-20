1829fc24fc050df7767927f8076bbe0a71720b26
VULKAN <vulkan.dev@protonmail.com>
2021-07-20 09:26:01 +0530

    implemented react-router-dom

--- DistroBoard/client/src/App.js
+++ DistroBoard/client/src/App.js
@@ -1,36 +1,27 @@
-import React, {useEffect, useState} from 'react';
-import {Grow, Grid, Container} from '@material-ui/core';
+import React from 'react';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
 
-import {useDispatch} from 'react-redux';
-import {getDistros} from './actions/distros';
+
 
 import Header from './components/Header/Header';
-import Distros from './components/Distros/Distros';
+import AllDistros from './components/AllDistros/AllDistros';
+import About from './components/About/About';
+import Auth from './components/Auth/Auth';
 
 const App = () => {
 
-  const [currentId, setCurrentId] = useState(null);
-
-  const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(getDistros());
-  }, [currentId, dispatch]);
-
   return (
-    <>
+    <BrowserRouter>
       <Header />
-      <Container maxWidth="lg">
-        <Grow in>
-          <Container>
-            <Grid container justify="space-between" alignItems="stretch" spacing={3}>
-              <Grid item xs={12} sm={12} md={12}>
-                <Distros setCurrentId={setCurrentId} />
-              </Grid>
-            </Grid>
-          </Container>
-        </Grow>
-      </Container>
-    </>
+
+      <Switch>
+        <Route path="/" exact component={AllDistros} />
+        <Route path="/about" exact component={About} />
+        <Route path="/auth" exact component={Auth} />
+
+      </Switch>
+
+    </BrowserRouter>
   );
 };
 
--- DistroBoard/client/src/components/Header/Header.js
+++ DistroBoard/client/src/components/Header/Header.js
@@ -1,5 +1,7 @@
 import React, {useState} from 'react';
 import {AppBar, Toolbar, Typography, IconButton, Tooltip, Avatar, Menu, MenuItem} from '@material-ui/core';
+import {Link} from 'react-router-dom';
+
 import PopupState, {bindTrigger, bindMenu} from 'material-ui-popup-state';
 
 import Search from "../Search/Search";
@@ -9,10 +11,10 @@ import useStyles from './styles';
 import Form from "../Form/Form";
 
 import GitHubIcon from '@material-ui/icons/GitHub';
-import CasinoIcon from '@material-ui/icons/Casino';
 import AddCircleIcon from '@material-ui/icons/AddCircle';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import InfoIcon from '@material-ui/icons/Info';
 
 const Header = () => {
   const classes = useStyles();
@@ -21,15 +23,15 @@ const Header = () => {
     <>
       <AppBar style={{marginBottom: 30}} color="default" position="sticky" >
         <Toolbar>
-          <Avatar alt="Logo" className={classes.logo} variant="square" src={logo} />
-          <Typography variant="h6" className={classes.responsive} style={{flexGrow: 1}}>DistroBoard</Typography>
+          <Avatar alt="Logo" component={Link} to="/" className={classes.logo} variant="square" src={logo} />
+          <Typography variant="h6" component={Link} to="/" className={classes.responsive} style={{flexGrow: 1, textDecoration: 'none', color: 'inherit'}}>DistroBoard</Typography>
 
           <Search />
 
           <div className={classes.responsive}  >
-            <Tooltip title="Distro Picker" aria-label="distro-picker"><IconButton color="inherit"><CasinoIcon /></IconButton></Tooltip>
             <Tooltip title="Add Distro" aria-label="add distro"><IconButton color="inherit" onClick={() => setOpenPopup(true)}><AddCircleIcon /></IconButton></Tooltip>
-            <Tooltip title="Login/Signup" aria-label="login/signup"><IconButton color="inherit"><AccountCircleIcon /></IconButton></Tooltip>
+            <Tooltip title="Login/Signup" aria-label="login/signup"><IconButton component={Link} to="/auth" color="inherit"><AccountCircleIcon /></IconButton></Tooltip>
+            <Tooltip title="About Us" aria-label="about"><IconButton color="inherit" component={Link} to="/about"><InfoIcon /></IconButton></Tooltip>
             <Tooltip title="View Source" aria-label="source"><IconButton color="inherit" href="https://github.com/DistroBoard/DistroBoard" target="_blank"><GitHubIcon /></IconButton></Tooltip>
           </div>
 
@@ -41,10 +43,10 @@ const Header = () => {
                 </IconButton>
                 <Menu {...bindMenu(popupState)}>
 
-                  <MenuItem onClick={() => popupState.close()}><CasinoIcon className={classes.menuIcons} fontSize="small" /> Distro Picker</MenuItem>
-                  <MenuItem onClick={() => {popupState.close(); window.open("https://github.com/DistroBoard/DistroBoard", "_blank");}}><GitHubIcon className={classes.menuIcons} fontSize="small" /> Visit Source</MenuItem>
+                  <MenuItem component={Link} to="/auth" onClick={() => popupState.close()}><AccountCircleIcon className={classes.menuIcons} fontSize="small" />Login/Signup</MenuItem>
                   <MenuItem onClick={() => {popupState.close(); setOpenPopup(true);}}><AddCircleIcon className={classes.menuIcons} fontSize="small" />Add Distro</MenuItem>
-                  <MenuItem onClick={() => popupState.close()}><AccountCircleIcon className={classes.menuIcons} fontSize="small" />Login/Signup</MenuItem>
+                  <MenuItem component={Link} to="/about" onClick={() => popupState.close()}><InfoIcon className={classes.menuIcons} fontSize="small" />About Us</MenuItem>
+                  <MenuItem onClick={() => {popupState.close(); window.open("https://github.com/DistroBoard/DistroBoard", "_blank");}}><GitHubIcon className={classes.menuIcons} fontSize="small" /> Visit Source</MenuItem>
                 </Menu>
               </div>
             )}
