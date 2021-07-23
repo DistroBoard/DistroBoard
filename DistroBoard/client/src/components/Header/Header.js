b13c89b9085abe1c497e96192ba154670ea5458f
VULKAN <vulkan.dev@protonmail.com>
2021-07-23 20:10:17 +0530

    imported MenuDrawer

--- DistroBoard/client/src/components/Header/Header.js
+++ DistroBoard/client/src/components/Header/Header.js
@@ -1,9 +1,10 @@
 import React, {useState} from 'react';
-import {AppBar, Toolbar, Typography, Button, IconButton, Tooltip, Avatar, Drawer, List, ListItem, ListItemText, ListItemIcon, ListSubheader, Divider} from '@material-ui/core';
+import {AppBar, Toolbar, Typography, Button, IconButton, Tooltip, Avatar} from '@material-ui/core';
 import {Link} from 'react-router-dom';
 
 
 import Search from "../Search/Search";
+import MenuDrawer from "../MenuDrawer/MenuDrawer";
 import logo from "../../assets/images/logo.png";
 import useStyles from './styles';
 
@@ -13,15 +14,7 @@ import GitHubIcon from '@material-ui/icons/GitHub';
 import AddCircleIcon from '@material-ui/icons/AddCircle';
 import InfoIcon from '@material-ui/icons/Info';
 import MenuIcon from '@material-ui/icons/Menu';
-import HomeIcon from '@material-ui/icons/Home';
-import WhatshotIcon from '@material-ui/icons/Whatshot';
-import ShuffleIcon from '@material-ui/icons/Shuffle';
-import SearchIcon from '@material-ui/icons/Search';
-import LocalOfferIcon from '@material-ui/icons/LocalOffer';
-import VpnKeyIcon from '@material-ui/icons/VpnKey';
-import ExitToAppIcon from '@material-ui/icons/ExitToApp';
-import MessageIcon from '@material-ui/icons/Message';
-import EmailIcon from '@material-ui/icons/Email';
+
 
 
 const Header = () => {
@@ -58,90 +51,7 @@ const Header = () => {
       </AppBar >
 
       <Form openPopup={openPopup} setOpenPopup={setOpenPopup} ></Form>
-
-      <Drawer open={openDrawer} onClose={() => setOpenDrawer(false)}>
-        <List disablePadding className={classes.drawer}>
-          <Toolbar />
-          <ListSubheader component="div" id="explore" disableSticky={true} >
-            Explore
-          </ListSubheader>
-
-          <ListItem button onClick={() => setOpenDrawer(false)} >
-            <ListItemIcon><HomeIcon /></ListItemIcon>
-            <ListItemText primary="Home" />
-          </ListItem>
-          <ListItem button onClick={() => setOpenDrawer(false)} >
-            <ListItemIcon><WhatshotIcon /></ListItemIcon>
-            <ListItemText primary="Trending" />
-          </ListItem>
-          <ListItem button onClick={() => setOpenDrawer(false)} >
-            <ListItemIcon><ShuffleIcon /></ListItemIcon>
-            <ListItemText primary="Random" />
-          </ListItem>
-          <ListItem button onClick={() => setOpenDrawer(false)} >
-            <ListItemIcon><LocalOfferIcon /></ListItemIcon>
-            <ListItemText primary="Browse" />
-          </ListItem>
-          <ListItem button onClick={() => setOpenDrawer(false)} >
-            <ListItemIcon><SearchIcon /></ListItemIcon>
-            <ListItemText primary="Search" />
-          </ListItem>
-          <Divider />
-
-          <ListSubheader component="div" id="account" disableSticky={true}>
-            Account
-          </ListSubheader>
-
-          <ListItem button onClick={() => setOpenDrawer(false)} >
-            <ListItemIcon><ExitToAppIcon /></ListItemIcon>
-            <ListItemText primary="Login" />
-          </ListItem>
-          <ListItem button onClick={() => setOpenDrawer(false)} >
-            <ListItemIcon><VpnKeyIcon /></ListItemIcon>
-            <ListItemText primary="Super User" />
-          </ListItem>
-
-
-          <Divider />
-          <ListSubheader component="div" id="social" disableSticky={true}>
-            Socials
-          </ListSubheader>
-
-          <ListItem button onClick={() => setOpenDrawer(false)} >
-            <ListItemIcon><GitHubIcon /></ListItemIcon>
-            <ListItemText primary="GitHub" />
-          </ListItem>
-          <ListItem button onClick={() => setOpenDrawer(false)} >
-            <ListItemIcon>< MessageIcon /></ListItemIcon>
-            <ListItemText primary="Matrix Chat" />
-          </ListItem>
-          <ListItem button onClick={() => setOpenDrawer(false)} >
-            <ListItemIcon>< EmailIcon /></ListItemIcon>
-            <ListItemText primary="Contact Us" />
-          </ListItem>
-
-          <Divider />
-          <ListSubheader component="div" id="more" disableSticky={true}>
-            More
-          </ListSubheader>
-
-          <ListItem button onClick={() => setOpenDrawer(false)} >
-            <ListItemIcon>< AddCircleIcon /></ListItemIcon>
-            <ListItemText primary="Submit your distro" />
-          </ListItem>
-          <ListItem button onClick={() => setOpenDrawer(false)} >
-            <ListItemIcon>< InfoIcon /></ListItemIcon>
-            <ListItemText primary="About Us" />
-          </ListItem>
-          <ListItem button onClick={() => setOpenDrawer(false)} >
-            <ListItemIcon>< AddCircleIcon /></ListItemIcon>
-            <ListItemText primary="Add Distro" />
-          </ListItem>
-
-
-
-        </List>
-      </Drawer>
+      <MenuDrawer openDrawer={openDrawer} setOpenDrawer={setOpenDrawer}></MenuDrawer>
     </>
   );
 }
